1f7b9be51402b723c100cbbd47052efd04cbccf7
Vlad Shmyhlo <aion.stu@gmail.com>
2018-07-06 14:11:28 +0300

    Refactor

--- train.py
+++ train.py
@@ -12,6 +12,8 @@ def build_summary(image, labels, logits):
     logits = tf.argmax(logits, -1)
     logits = tf.expand_dims(logits, -1)
 
+    labels = tf.Print(labels, [tf.reduce_min(labels), tf.reduce_max(labels)])
+
     tf.summary.image('image', image)
     tf.summary.image('mask_true', tf.image.convert_image_dtype(labels, tf.uint8))
     tf.summary.image('mask_pred', tf.image.convert_image_dtype(logits, tf.uint8))
